0e5e0dcb7cedd7e6aaf1dca7f5126df443b5eaca
AugGust <piggy013130@yahoo.com.sg>
2019-08-28 21:01:03 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
 	static Scanner sc;
-	static ArrayList list;
+	static ArrayList<String> list;
 	
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -12,6 +12,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
+    	list = new ArrayList<String>();
     	System.out.println("	____________________________________________________________");
     	System.out.println("	Hello! I'm Duke");
     	System.out.println("	What can I do for you?");
@@ -29,8 +30,17 @@ public class Duke {
     		System.out.println("	Bye. Hope to see you again soon!");
     		System.out.println("	____________________________________________________________\n	");
     	}
+    	else if (test.equals("list"))	{
+        	for (int i = 0; i < list.size(); i++)	{
+        		System.out.print("	" + (i+1) + ". ");
+        		System.out.println("	" + list.get(i));
+        	}
+    		System.out.println("	____________________________________________________________\n	");
+    		nextCommand();
+    	}
     	else	{
-    		System.out.println('	' + test);
+    		list.add(test);
+    		System.out.println('	' + "added: " + test);
         	System.out.println("	____________________________________________________________");
         	nextCommand();
     	}
