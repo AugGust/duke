3e07763d055241845b4780560306d038dc969d3f
AugGust <piggy013130@yahoo.com.sg>
2019-08-28 21:26:36 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
 	static Scanner sc;
-	static ArrayList<String> list;
+	static ArrayList<Task> list;
 	
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -12,7 +12,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-    	list = new ArrayList<String>();
+    	list = new ArrayList<Task>();
     	System.out.println("	____________________________________________________________");
     	System.out.println("	Hello! I'm Duke");
     	System.out.println("	What can I do for you?");
@@ -33,16 +33,46 @@ public class Duke {
     	else if (test.equals("list"))	{
         	for (int i = 0; i < list.size(); i++)	{
         		System.out.print("	" + (i+1) + ". ");
-        		System.out.println("	" + list.get(i));
+        		System.out.println("	[" + list.get(i).getStatusIcon() + "] " + list.get(i).getTask());
         	}
     		System.out.println("	____________________________________________________________\n	");
     		nextCommand();
     	}
+    	else if (test.substring(0, 4).equals("done")) {
+    		int index = Integer.parseInt(test.substring(5, 6)) - 1;
+    		list.get(index).markAsDone();
+    		System.out.println("	Nice! I've marked this task as done:");
+    		System.out.println("	[" + list.get(index).getStatusIcon() + "] " + list.get(index).getTask());
+    		System.out.println("	____________________________________________________________");
+    		nextCommand();
+    	}
     	else	{
-    		list.add(test);
+    		list.add(new Task(test));
     		System.out.println('	' + "added: " + test);
         	System.out.println("	____________________________________________________________");
         	nextCommand();
+    	}
+    }
+    
+    public static class Task {
+    	protected String task;
+    	protected boolean isDone;
+    	
+    	public Task(String task)	{
+    		this.task = task;
+    		this.isDone = false;
+    	}
+    	
+    	public String getStatusIcon()	{
+    		return (isDone ? "✓" : "✗");
+    	}
+    	
+    	public void markAsDone()	{
+    		this.isDone = true;
+    	}
+    	
+    	public String getTask()	{
+    		return this.task;
     	}
     }
 }
